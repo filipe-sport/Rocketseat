c67a74defedcedf2dc00d78c4c2b50e60492631c
Filipe Cesar <106756536+filipe-sport@users.noreply.github.com>
2023-01-22 22:03:59 -0300

    update

---
+++ JAVASCRIPT/Funções/funcoes.js
@@ -0,0 +1,21 @@
+// Criar um aplicativo de frases motivacionais
+
+console.log("Estudar é muito bom");
+console.log("Pacieência e Persistência");
+console.log("Revisão é mãe do aprendizado");
+
+console.log("--------".repeat(10));
+
+//declaration - declaração da função
+// function statement
+
+function createPhrases(){
+    console.log("Estudar é muito bom");
+    console.log("Pacieência e Persistência");
+    console.log("Revisão é mãe do aprendizado");
+}
+
+//execute! a função, run, call, invoke
+
+createPhrases();
+createPhrases();
