6baa62d6237765eeae83b836af197c216a75db11
Filipe Cesar <106756536+filipe-sport@users.noreply.github.com>
2023-01-26 14:06:26 -0300

    update files

--- JAVASCRIPT/Consolidando exercícios/fluxo de caixa familiar.js
+++ JAVASCRIPT/Consolidando exercícios/fluxo de caixa familiar.js
@@ -8,3 +8,26 @@ Agora, crie uma função que irá calcular o total de receitas e
 despesas e irá mostrar uma mensagem se a família está com 
 saldo positivo ou negativo, seguido do valor do saldo.
 */
+let receitas = [1600,1300,260,600];
+let despesas = [510, 1800, 120, 600, 350, 500]
+
+totalReceitas = function(){
+    let total = 0
+    for(let i=0; i<receitas.length; i++){
+        total += receitas[i]
+    }
+    return total
+}
+totalDespesas = function(){
+    let total = 0
+    for(let j=0; j<despesas.length; j++){
+        total += despesas[j]
+    }
+    return total
+}
+function totalGeral(){
+    total = totalReceitas()-totalDespesas()
+    result = (total<0) ? (`Saldo negativo no valor de: ${total}`) : (`Saldo positivo no valor de: ${total}`)
+    console.log(result);
+}
+totalGeral()
