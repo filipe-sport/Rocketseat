e04de4373efc75654db940efe09a1b61ab5c1e01
Filipe <filipecesarufpe@gmail.com>
2023-02-27 10:43:02 -0300

    update files

--- Avançado/Trabalhando com API/index.js
+++ Avançado/Trabalhando com API/index.js
@@ -1,10 +1,10 @@
-// CRIANDO SERVER
+// CRIANDO SERVER (para instalar os node_modules rodar => npm i express)
 const express = require('express');
-const res = require('express/lib/response');
-const app = express();
-app.listen('3000')
 
-// ROTA GET
+const app = express();
+app.listen('3030') // verificar sempre a disponibilidade de porta
+
+// REQUISIÇÃO GET
 // app.route('/').get((req,res) => res.send('hello'))
 app.route('/sobre').get((req,res) => res.send('hello sobre'))
 
@@ -12,13 +12,21 @@ app.route('/sobre').get((req,res) => res.send('hello sobre'))
 app.use(express.json())
 
 
-// ROTA POST
+// REQUISIÇÃO POST
 app.route('/').post((req,res) => res.send(req.body))
 
-//ROTA PUT (PUT - serve pra editar requisições)
+// REQUISIÇÃO PUT (PUT - serve pra editar requisições)
 let author = "Filipe"
 app.route('/').get((req,res) => res.send(author))
 app.route('/').put((req,res) => {
     author = req.body.author
     res.send(author)
 })
+
+// REQUISIÇÃO DELETE
+app.route('/:identificador').delete((req,res) => {
+    res.send(req.params.identificador)
+})
+
+// PARAR O SERVIDOR 
+// Apertar Ctrl + C
