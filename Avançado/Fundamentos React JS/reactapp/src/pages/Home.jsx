c7a51ab9f35b15e7ebe491d0889027ecec713241
Filipe Cesar <106756536+filipe-sport@users.noreply.github.com>
2023-03-06 18:50:18 -0300

    update files

--- Avançado/Fundamentos React JS/reactapp/src/pages/Home.jsx
+++ Avançado/Fundamentos React JS/reactapp/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 function Home() {
   return (
-    <h1>Lista de Presença</h1>    
+    <div>
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite o nome..." />    
+      <button type='button'>Adicionar</button>
+    </div>
   )
 }
 
