85426383b497e5f4df9bbeca1265b3475b42e288
Filipe <filipecesarufpe@gmail.com>
2023-03-08 01:09:52 -0300

    update files

--- Avançado/Fundamentos React JS/reactapp/src/pages/Home/index.jsx
+++ Avançado/Fundamentos React JS/reactapp/src/pages/Home/index.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
+import "./style.css"
 export function Home() {
   return (
-    <div>
+    <div className='container'>
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome..." />    
       <button type='button'>Adicionar</button>
