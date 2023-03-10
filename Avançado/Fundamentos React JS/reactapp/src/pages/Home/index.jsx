02af23efaf82529466c465dc3e19c781d00aefb8
Filipe <filipecesarufpe@gmail.com>
2023-03-10 10:35:09 -0300

    update files

--- Avançado/Fundamentos React JS/reactapp/src/pages/Home/index.jsx
+++ Avançado/Fundamentos React JS/reactapp/src/pages/Home/index.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import "./style.css"
+import { Card } from '../../components/Card';
+
+
 export function Home() {
+  const [studentName, setStudentName] = useState('')
+  const [students, setStudents] = useState([])
   return (
     <div className='container'>
       <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome..." />    
+      <input type="text" placeholder="Digite o nome..." 
+        onChange={e => setStudentName(e.target.value)}
+      />    
       <button type='button'>Adicionar</button>
+
+    {/* Usando os components */}
+    
+    {
+      <Card name = "Rodrigo" time = "10:55:25" />
+    }  
     </div>
   )
 }
