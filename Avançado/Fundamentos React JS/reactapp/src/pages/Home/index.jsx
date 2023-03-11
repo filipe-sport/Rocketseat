dd5c5156f6790f0944c66fa62fc3668aace42fcb
Filipe <filipecesarufpe@gmail.com>
2023-03-11 00:08:16 -0300

    update files

--- Avançado/Fundamentos React JS/reactapp/src/pages/Home/index.jsx
+++ Avançado/Fundamentos React JS/reactapp/src/pages/Home/index.jsx
@@ -6,18 +6,38 @@ import { Card } from '../../components/Card';
 export function Home() {
   const [studentName, setStudentName] = useState('')
   const [students, setStudents] = useState([])
+
+  function handleAddStudent (){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+      })
+    }
+    setStudents(prevState => [...prevState, newStudent])
+  }
+
+
   return (
     <div className='container'>
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome..." 
         onChange={e => setStudentName(e.target.value)}
       />    
-      <button type='button'>Adicionar</button>
+      <button 
+        type='button' onClick={handleAddStudent}>Adicionar
+      </button>
 
     {/* Usando os components */}
     
     {
-      <Card name = "Rodrigo" time = "10:55:25" />
+
+      students.map(student => (
+        <Card name = {student.name} time = {student.time} />
+      ))
+      
     }  
     </div>
   )
